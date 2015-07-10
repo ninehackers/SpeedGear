89128a854a87f3a840d978e779b1e395549c6bae
LambdaTea <mousehaohao@hotmail.com>
2015-07-10 18:50:52 +0800

    Rejected commit: Default

---
+++ SpeedGear-1.0/app/src/main/java/com/ninehackers/speedgear/stanley/StanleyDemo.java
@@ -0,0 +1,13 @@
+package com.ninehackers.speedgear.stanley;
+
+/**
+ * Created by Administrator on 2015/7/10.
+ */
+public class StanleyDemo {
+
+    public boolean IsStanley() {
+        return true;
+    }
+
+
+}
