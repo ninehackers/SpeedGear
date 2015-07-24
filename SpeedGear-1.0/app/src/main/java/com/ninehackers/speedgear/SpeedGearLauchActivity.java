0d0bd845f56c2005f5ab6e1f070030ac167920e7
dabaosod011 <dabaosod011@gmail.com>
2015-07-24 16:09:54 +0800

    add the connection between agent and server

--- SpeedGear-1.0/app/src/main/java/com/ninehackers/speedgear/SpeedGearLauchActivity.java
+++ SpeedGear-1.0/app/src/main/java/com/ninehackers/speedgear/SpeedGearLauchActivity.java
@@ -1,17 +1,31 @@
 package com.ninehackers.speedgear;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.ninehackers.speedgear.ui.SpeedGearSettingFragment;
 import com.ninehackers.speedgear.utils.StringUtil;
 import com.ninehackers.speedgear.utils.data.ConfigData;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
+import static java.lang.Thread.*;
 
 
 public class SpeedGearLauchActivity extends ActionBarActivity {
+    private static String LOG_TAG = "SpeedGearLauchActivity";
+    private Button btnPingServer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,21 +70,89 @@ public class SpeedGearLauchActivity extends ActionBarActivity {
     }
 
     private void initComponents() {
-        ConfigData configData = new ConfigData(getApplicationContext());
+        btnPingServer = (Button) this.findViewById(R.id.btn_ping_server);
 
-        String server_address = configData.getSpeedgearServerAddress();
-        String server_port = configData.getSpeedgearServerPort();
-        String username = configData.getSpeedgearUsername();
+        btnPingServer.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                Log.d("Log", "click on btnPingServer");
 
-        if (StringUtil.isEmpty(server_address) ||
-                StringUtil.isEmpty(server_port) ||
-                StringUtil.isEmpty(username)) {
-            try {
-                SpeedGearSettingFragment sgSettingDialog = new SpeedGearSettingFragment();
-                sgSettingDialog.show(getFragmentManager(), "dialog");
-            } catch (Exception e) {
-                e.printStackTrace();
+                ConfigData configData = new ConfigData(getApplicationContext());
+                String server_address = configData.getSpeedgearServerAddress();
+                String server_port = configData.getSpeedgearServerPort();
+                String username = configData.getSpeedgearUsername();
+
+                if (StringUtil.isEmpty(server_address) ||
+                        StringUtil.isEmpty(server_port) ||
+                        StringUtil.isEmpty(username)) {
+                    try {
+                        SpeedGearSettingFragment sgSettingDialog = new SpeedGearSettingFragment();
+                        sgSettingDialog.show(getFragmentManager(), "dialog");
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    new pingServerTask(server_address, server_port, username).execute();
+                }
             }
+        });
+    }
+
+
+    private class pingServerTask extends AsyncTask<Void, Void, Boolean> {
+        private String server_address = null;
+        private String server_port = null;
+        private String username = null;
+        private String greetingUrl = null;
+
+        pingServerTask(String s_address, String s_port, String uname) {
+            server_address = s_address;
+            server_port = s_port;
+            username = uname;
+            greetingUrl = "http://" + server_address + ":" + server_port + "/greeting/" + username;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            OkHttpClient httpClient = new OkHttpClient();
+            if (null != greetingUrl) {
+                try {
+                    Request request = new Request.Builder().url(greetingUrl).build();
+                    Response response = httpClient.newCall(request).execute();
+                    String responseBody = response.body().string();
+
+                    Log.d(LOG_TAG, "response is: " + response.toString());
+                    Log.d(LOG_TAG, "responseBody is: " + responseBody);
+
+                    //  need to check both response code and body,
+                    //  because the http request maybe hijacked by search engine or ISP
+                    if (response != null
+                            && response.code() == 200
+                            && responseBody.indexOf(username) != -1) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return false;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            btnPingServer.setClickable(false);
+            btnPingServer.setEnabled(false);
+            //btnPingServer.setOnClickListener(null);
+        }
+
+        protected void onPostExecute(Boolean result) {
+            Toast.makeText(getApplicationContext(), result ? R.string.ping_server_success : R.string.ping_server_falied,
+                    Toast.LENGTH_SHORT).show();
+            btnPingServer.setClickable(true);
+            btnPingServer.setEnabled(true);
         }
     }
 }
--- SpeedGear-1.0/app/src/main/java/com/ninehackers/speedgear/ui/SpeedGearSettingFragment.java
+++ SpeedGear-1.0/app/src/main/java/com/ninehackers/speedgear/ui/SpeedGearSettingFragment.java
@@ -10,9 +10,9 @@ import android.os.Bundle;
 import android.widget.EditText;
 
 import com.ninehackers.speedgear.R;
-
 import com.ninehackers.speedgear.utils.StringUtil;
 import com.ninehackers.speedgear.utils.data.ConfigData;
+
 
 /**
  * Created by dabaosod011 on 2015/7/7.
@@ -50,32 +50,32 @@ public class SpeedGearSettingFragment extends DialogFragment {
         EditText usernameText = (EditText) dialogView.findViewById(R.id.username_id);
 
         ConfigData configData = new ConfigData(getActivity());
-        String server_address = configData.getSpeedgearServerAddress();
-        String server_port = configData.getSpeedgearServerPort();
-        String username = configData.getSpeedgearUsername();
+        String serverAddress = configData.getSpeedgearServerAddress();
+        String serverPort = configData.getSpeedgearServerPort();
+        String userName = configData.getSpeedgearUsername();
 
-        if (!StringUtil.isEmpty(server_address)) {
-            serverAddressText.setText(server_address);
+        if (!StringUtil.isEmpty(serverAddress)) {
+            serverAddressText.setText(serverAddress);
         }
 
-        if (!StringUtil.isEmpty(server_port)) {
-            serverPortText.setText(server_port);
+        if (!StringUtil.isEmpty(serverPort)) {
+            serverPortText.setText(serverPort);
         }
 
-        if (!StringUtil.isEmpty(username)) {
-            usernameText.setText(username);
+        if (!StringUtil.isEmpty(userName)) {
+            usernameText.setText(userName);
             usernameText.setEnabled(false);
         }
     }
 
     private void saveSettings() {
-        String server_address = ((EditText) (dialogView.findViewById(R.id.server_address_id))).getText().toString();
-        String server_port = ((EditText) (dialogView.findViewById(R.id.server_port_id))).getText().toString();
-        String username = ((EditText) (dialogView.findViewById(R.id.username_id))).getText().toString();
+        String serverAddress = ((EditText) (dialogView.findViewById(R.id.server_address_id))).getText().toString();
+        String serverPort = ((EditText) (dialogView.findViewById(R.id.server_port_id))).getText().toString();
+        String userName = ((EditText) (dialogView.findViewById(R.id.username_id))).getText().toString();
 
         ConfigData configData = new ConfigData(getActivity());
-        configData.setSpeedgearServerAddress(server_address);
-        configData.setSpeedgearServerPort(server_port);
-        configData.setSpeedgearUsername(username);
+        configData.setSpeedgearServerAddress(serverAddress);
+        configData.setSpeedgearServerPort(serverPort);
+        configData.setSpeedgearUsername(userName);
     }
 }
